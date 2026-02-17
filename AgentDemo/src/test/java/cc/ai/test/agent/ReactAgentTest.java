7431453944c3042a1f5fbc97cb82f0acd690f541
wc <wangchao5149@outlook.com>
2026-02-17 22:58:19 +0800

    王超：创建demo示例集合

---
+++ AgentDemo/src/test/java/cc/ai/test/agent/ReactAgentTest.java
@@ -0,0 +1,16 @@
+package cc.ai.test.agent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ReactAgentTest {
+
+    @Test
+    public void test_01() {
+        System.out.println("test_01");
+    }
+}
